7b9d199b26ddd22747e64417e92dfe4d666fc343
renehammer <rene_hammer@web.de>
2025-04-16 10:07:57 +0200

    Add files via upload

--- telegram_config.py
+++ telegram_config.py
@@ -3,7 +3,7 @@
 import requests
 
 BOT_TOKEN = "8126985237:AAGKurwSf_zv263XY2FmYladow6cH05o1e8"
-CHAT_ID = None  # Muss beim ersten Start gesetzt werden
+CHAT_ID = 7428599123
 
 def send_telegram_message(message, chat_id=CHAT_ID):
     if chat_id is None:
